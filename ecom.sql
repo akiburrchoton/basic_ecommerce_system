fd6e424ce6d28571e17067387898b3aefffcf3a3
Akibur <akibur.choton@gmail.com>
2020-06-25 21:37:26 +0600

    udpated database - 25/06/2020

--- ecom.sql
+++ ecom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2020 at 12:19 PM
+-- Generation Time: Jun 25, 2020 at 05:36 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -32,7 +32,7 @@ CREATE TABLE `categories` (
   `Cat_id` int(11) NOT NULL,
   `Cat_name` varchar(255) NOT NULL,
   `Cat_description` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  `Cat_parent` int(11) NOT NULL COMMENT 'Identify Parent and Child',
+  `Cat_parent` int(11) NOT NULL COMMENT 'Identify Parent and Child (If 0 -> Parent Category)',
   `Cat_ordering` int(11) NOT NULL,
   `Cat_visibility` int(11) NOT NULL,
   `Cat_comments` int(11) NOT NULL,
@@ -46,12 +46,35 @@ CREATE TABLE `categories` (
 INSERT INTO `categories` (`Cat_id`, `Cat_name`, `Cat_description`, `Cat_parent`, `Cat_ordering`, `Cat_visibility`, `Cat_comments`, `Cat_ads`) VALUES
 (1, 'Smartphone', 'This is Smart phone category. Any kind of smart can be included in this category.', 0, 1, 0, 1, 0),
 (2, 'Computer', 'In here any kind of computer can be added in here. ', 0, 2, 0, 0, 1),
-(3, 'Headphone', 'This is the category where any kinds of headphone can be added. ', 0, 3, 0, 1, 0),
-(8, 'New Headphone', '', 3, 1, 0, 1, 0),
+(3, 'Motor Bike', 'This is the category where any kinds of Motor Bike can be added. ', 0, 3, 0, 1, 0),
+(8, 'Honda', '', 3, 1, 0, 1, 0),
 (9, 'Game', '', 0, 4, 1, 0, 0),
 (10, 'Fighting Game', 'Fighting Game', 9, 7, 1, 1, 1),
 (11, 'Alienware Laptop', 'Grey Alienware Laptop', 2, 5, 1, 1, 1),
 (12, 'Samsung', 'Sub category of Smartphone Category', 1, 6, 0, 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `comment_status` tinyint(4) NOT NULL,
+  `comment_date` date NOT NULL,
+  `comment_itemid` int(11) NOT NULL,
+  `comment_userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `comment`, `comment_status`, `comment_date`, `comment_itemid`, `comment_userid`) VALUES
+(1, 'This is a very good smartphone to use. 4.5 Years using with no problem. Great one!', 0, '2020-05-11', 1, 1),
+(2, 'This is a very good smartphone to use. 4.5 Years using with no problem. Great one!', 0, '2020-05-11', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -62,15 +85,27 @@ INSERT INTO `categories` (`Cat_id`, `Cat_name`, `Cat_description`, `Cat_parent`,
 CREATE TABLE `items` (
   `Item_ID` int(11) NOT NULL,
   `Item_name` varchar(255) NOT NULL,
-  `Item_description` int(255) NOT NULL,
+  `Item_description` varchar(255) NOT NULL,
   `Item_price` int(11) NOT NULL,
   `Item_date` date NOT NULL,
   `Item_country` varchar(255) NOT NULL,
   `Item_status` int(11) NOT NULL,
+  `Item_rating` smallint(6) NOT NULL,
+  `Cat_ID` int(11) NOT NULL,
   `User_ID` int(11) NOT NULL,
-  `Item_approval` int(11) NOT NULL,
-  `Item_image` varchar(255) NOT NULL
+  `Item_image` varchar(255) NOT NULL,
+  `Item_approval` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`Item_ID`, `Item_name`, `Item_description`, `Item_price`, `Item_date`, `Item_country`, `Item_status`, `Item_rating`, `Cat_ID`, `User_ID`, `Item_image`, `Item_approval`) VALUES
+(1, 'Samsung S6 Edge', 'Black Samsung S6 Edge\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry.', 600, '2020-04-04', 'Vietnam', 4, 0, 12, 1, '', 1),
+(2, 'Honda Hornet', 'Green Honda Hornet 160cc\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry.', 150000, '2020-04-04', 'Japan', 2, 0, 8, 2, '', 1),
+(3, 'Alienware 13', 'Alienware 13 R2 Grey\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry.', 90000, '2020-04-04', 'Australia', 3, 0, 11, 1, '', 1),
+(9, 'Redmi ', 'Redmi Smart Phone orem Ipsum is simply dummy text of the printing and typesetting industry.', 123, '2020-04-28', 'BD', 2, 0, 1, 2, '', 1);
 
 -- --------------------------------------------------------
 
@@ -98,10 +133,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`UserID`, `Username`, `Password`, `FullName`, `Email`, `PhoneNumber`, `PAddress`, `RegStatus`, `GroupID`, `Date`, `Avatar`) VALUES
 (1, 'tandra', '601f1889667efaebb33b8c12572835da3f027f78', 'Tandra Zaman', 'tandra@gmail.com', 1683848060, 'Uttara Dhaka', 1, 1, '2020-03-05', '733474_sultanazaman.jpg'),
-(17, 'akibur', '601f1889667efaebb33b8c12572835da3f027f78', 'Akibur Rahman', 'akibur.choton@gmail.com', 2147483647, 'Road-16, Sector-14.', 1, 1, '2020-03-08', ''),
-(27, 'akiburrchoton', '601f1889667efaebb33b8c12572835da3f027f78', 'Akibur Rahman', 'akibur.choton@gmail.com', 1616103020, 'Road-16, Sector-14, Uttara', 1, 0, '2020-03-09', '344_akibur.jpg'),
-(28, 'Tandra1', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Sultana Zaman Tandra', 'tandrazaman90@gmail.com', 1683848060, 'Uttara', 1, 0, '2020-03-09', '733474_sultanazaman.jpg'),
-(44, 'Dummy', '27e95131edc7ed3c63472067ea1d95cf8f0056cf', 'Dummy Dummy', 'd@d.com', 123123, 'Uttara', 0, 0, '2020-03-27', '846732_Akibur.jpg');
+(2, 'akibur', '601f1889667efaebb33b8c12572835da3f027f78', 'Akibur Rahman', 'akibur.choton@gmail.com', 2147483647, 'Road-16, Sector-14.', 1, 1, '2020-03-08', ''),
+(3, 'akiburrchoton', '601f1889667efaebb33b8c12572835da3f027f78', 'Akibur Rahman', 'akibur.choton@gmail.com', 1616103020, 'Road-16, Sector-14, Uttara', 1, 0, '2020-03-09', '344_akibur.jpg'),
+(4, 'Tandra1', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Sultana Zaman Tandra', 'tandrazaman90@gmail.com', 1683848060, 'Uttara', 1, 0, '2020-03-09', '733474_sultanazaman.jpg'),
+(5, 'Dummy', '27e95131edc7ed3c63472067ea1d95cf8f0056cf', 'Dummy Dummy', 'd@d.com', 123123, 'Uttara', 0, 0, '2020-03-27', '846732_Akibur.jpg'),
+(45, 'test', '7c4a8d09ca3762af61e59520943dc26494f8941b', '', 'test@test.com', 0, '', 1, 0, '2020-04-24', '');
 
 --
 -- Indexes for dumped tables
@@ -112,6 +148,12 @@ INSERT INTO `users` (`UserID`, `Username`, `Password`, `FullName`, `Email`, `Pho
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`Cat_id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
 
 --
 -- Indexes for table `items`
@@ -136,16 +178,22 @@ ALTER TABLE `categories`
   MODIFY `Cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `Item_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Item_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Define User ID ', AUTO_INCREMENT=45;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Define User ID ', AUTO_INCREMENT=46;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
